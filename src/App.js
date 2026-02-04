032ed8d340b76daaf65f8b1fb59ae7aaa0c6577e
singhsunny26sep <101707682+singhsunny26sep@users.noreply.github.com>
2026-02-05 00:04:09 +0530

    newchnges

--- src/App.js
+++ src/App.js
@@ -60,6 +60,7 @@ function App() {
           <main className="main-content">
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/Home" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
               <Route path="/products" element={<Products />} />
